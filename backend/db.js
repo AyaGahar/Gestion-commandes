1bb8aeadb4dc8515bad9d1d60b70d48c2a5cca59
AyaGahar <158850670+AyaGahar@users.noreply.github.com>
2025-11-29 19:31:12 +0100

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,8 @@
 import mysql from "mysql2/promise";
 
 export const db = await mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "gest_commandes"
+    host: "",
+    user: "",
+    password: "",
+    database: ""
 });
